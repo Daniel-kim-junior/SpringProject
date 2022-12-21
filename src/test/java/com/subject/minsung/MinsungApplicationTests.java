7908138fc7c9d56282676b936e72e1b8f2c7966d
jio-kim <now20412041@gmail.com>
2022-12-21 23:37:29 +0900

    first Commit

---
+++ src/test/java/com/subject/minsung/MinsungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subject.minsung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinsungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
